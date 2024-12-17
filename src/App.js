e1c37539d7172028d9636f23fd6e6c347391d916
yashasj <yashas.j@dbizsolution.com>
2024-12-17 19:28:32 +0530

    added LoadMore Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Accordian from "./components/accordian/Accordian";
 import ImageSlider from "./components/imagesSlider/ImageSlider";
+import LoadMore from "./components/loadmore/LoadMore";
 import StarRating from "./components/starrating/StarRating";
 
 function App() {
   return (
     <div className="App">
-      <ImageSlider url="https://picsum.photos/v2/list" />
+      <LoadMore />
     </div>
   );
 }
